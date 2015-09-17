7d46237a0b683c130574dd6fcb5705747db72d2c
john <redford.john.m@gmail.com>
2015-09-17 08:31:58 -0500

    handles case of target not in array

--- RotArraySearch.js
+++ RotArraySearch.js
@@ -1,14 +1,17 @@
 function rotatedArraySearch (rotated, target) {
-  var found = false;
+  // Takes a rotated, ordered array and outputs the index of a target value
+  // E.g.  [5,7,9,2,4],2 -> 3
+  // Returns -1 if target is not in array
   var a = 0;
   var d = rotated.length - 1;
   var b = Math.floor(rotated.length/2);
   var c = b + 1;
-  while (!found) {
+  while (true) {
     if (rotated[a] === target) { return a }
     if (rotated[b] === target) { return b }
     if (rotated[c] === target) { return c }
     if (rotated[d] === target) { return d }
+    if (a > d - 3) { return -1 }
     if (( rotated[a] > target && rotated[b] > target && rotated[c] > target && rotated[d] > target && rotated[a] > rotated[b] ) ||
         ( rotated[a] < target && rotated[b] < target && rotated[c] < target && rotated[d] < target && rotated[a] > rotated[b] ) ||
         ( rotated[a] < target && rotated[b] > target && rotated[c] > target && rotated[d] < target )){
