d2fe9d439abc8e86d7b8abed457f4c5d8d5eec1b
john <redford.john.m@gmail.com>
2015-09-30 21:18:20 -0500

    toy problem to solve tomorrow

---
+++ mazeSolver.js
@@ -0,0 +1,10 @@
+// take an array of arrays containing the numbers zero or one, representing a maze
+// in which the acceptable locations are zeroes, the walls are ones, the upper
+// left corner is a zero and is the start, and the lower right corner is a zero
+// and is the finish.  Allowed moves are horizontal and vertical.
+
+var findSolution = function(maze){
+  var X = maze.length;
+  var Y = maze[0].length;
+  
+}
